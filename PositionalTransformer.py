30659c6ae1e5dc11ac7deb96b9e48c7a87c27bde
mjs227 <92041658+mjs227@users.noreply.github.com>
2023-08-30 12:53:24 -0400

    Update PositionalTransformer.py

--- PositionalTransformer.py
+++ PositionalTransformer.py
@@ -2,10 +2,41 @@ import torch
 import torch.nn as nn
 
 
-def dot(x, y):
-    out_t = torch.sum(x * y, dim=-1)
+def get_windows(in_ten, l_pad_size, r_pad_size, pad_tok=0):
+    _, in_len, pt_dim = tuple(in_ten.shape)
 
-    return torch.reshape(out_t, (out_t.shape[0], 1))
+    if pad_tok is None:
+        l_pad = in_ten[:, 0, :].repeat(1, l_pad_size, 1)
+        r_pad = in_ten[:, -1, :].repeat(1, r_pad_size, 1)
+        in_ten = in_ten[:, 1:-1, :]
+        in_len -= 2
+    else:
+        l_pad = torch.full(
+            (1, l_pad_size, pt_dim),
+            pad_tok,
+            device=DEVICE,
+            dtype=in_ten.dtype
+        )
+        r_pad = torch.full(
+            (1, r_pad_size, pt_dim),
+            pad_tok,
+            device=DEVICE,
+            dtype=in_ten.dtype
+        )
+
+    pad_len = in_len + l_pad_size + r_pad_size
+    indices = torch.arange(in_len, device=DEVICE).view((in_len, 1)).repeat((1, pad_len))
+    rng = torch.arange(pad_len, device=DEVICE).view((1, pad_len)).repeat((in_len, 1))
+    rng = ((rng + indices) % pad_len).unsqueeze(-1).expand(-1, -1, pt_dim)
+
+    in_ten = torch.cat((l_pad, in_ten, r_pad), dim=1).repeat(in_len, 1, 1)
+    in_ten = torch.gather(in_ten, 1, rng)
+
+    r_windows = in_ten[:, l_pad_size + 1:l_pad_size + r_pad_size + 1, :]
+    centers = in_ten[:, l_pad_size, :].unsqueeze(1)
+    l_windows = in_ten[:, :l_pad_size, :]
+
+    return torch.cat((l_windows, r_windows, centers), dim=1)
 
 
 class PTLayer(nn.Module):
@@ -13,90 +44,57 @@ class PTLayer(nn.Module):
         super(PTLayer, self).__init__()
         self.IN_DIM = in_dim
         self.WINDOW = window
-        self.q = nn.Linear(self.IN_DIM, self.IN_DIM)
-        self.k_prev = nn.ModuleList([nn.Linear(self.IN_DIM, self.IN_DIM) for _ in range(window)])
-        self.k_cent = nn.Linear(self.IN_DIM, self.IN_DIM)
-        self.k_next = nn.ModuleList([nn.Linear(self.IN_DIM, self.IN_DIM) for _ in range(window)])
-        self.ff_prev = nn.ModuleList([nn.Linear(self.IN_DIM, self.IN_DIM) for _ in range(window)])
-        self.ff_cent = nn.Linear(self.IN_DIM, self.IN_DIM)
-        self.ff_next = nn.ModuleList([nn.Linear(self.IN_DIM, self.IN_DIM) for _ in range(window)])
+        self.q = nn.Linear(in_dim, in_dim)
+        self.k = nn.Conv1d(window + 1, (window + 1) * in_dim, in_dim, stride=in_dim, groups=(window + 1))
+        self.ff_window = nn.Conv1d(window, window * in_dim, in_dim, stride=in_dim, groups=window)
+        self.ff_cent = nn.Linear(in_dim, in_dim)
         self.dropout = nn.Dropout(p=p)
- 
+
     def forward(self, x):
-        x_split = torch.split(x, self.IN_DIM, dim=1)
-        x_prev = list(x_split[:self.WINDOW])
-        x_cent = x_split[self.WINDOW]
-        x_next = list(x_split[self.WINDOW + 1:])
+        q = self.q(x[:, -1, :])
+        k = self.k(x).reshape(x.size(0), self.WINDOW + 1, self.IN_DIM)
+        attns = torch.matmul(q.unsqueeze(1), torch.transpose(k, -2, -1))
+        attns = torch.softmax(attns, dim=2)
 
-        q = self.q(x_cent)
-        x_cent_ = x_cent * dot(q, self.k_cent(x_cent))
+        x_cent = torch.matmul(attns, x)
+        x_cent = torch.nn.functional.normalize(x_cent, p=2, dim=2)
+        x_cent = self.dropout(self.ff_cent(x_cent))
+        x_cent = torch.nn.functional.normalize(x_cent + x[:, -1, :].unsqueeze(1), p=2, dim=2)
 
-        for i in range(self.WINDOW):
-            attn_prev_i = dot(q, self.k_prev[i](x_prev[i]))
-            attn_next_i = dot(q, self.k_next[i](x_next[i]))
-            x_cent_ = x_cent_ + (x_prev[i] * attn_prev_i) + (x_next[i] * attn_next_i)
-            
-            x_prev[i] = self.ff_prev[i](x_prev[i])
-            x_prev[i] = self.dropout(x_prev[i])
-            x_prev[i] = torch.nn.functional.normalize(x_prev[i], p=2, dim=1)
-            
-            x_next[i] = self.ff_next[i](x_next[i])
-            x_next[i] = self.dropout(x_next[i])
-            x_next[i] = torch.nn.functional.normalize(x_next[i], p=2, dim=1)
+        x_window = self.ff_window(x[:, :-1, :]).reshape(x.size(0), self.WINDOW, self.IN_DIM)
+        x_window = self.dropout(x_window + x[:, :-1, :])
+        x_window = torch.nn.functional.normalize(x_window, p=2, dim=2)
 
-        x_cent_ = torch.nn.functional.normalize(x_cent_, p=2, dim=1)
-        x_cent_ = self.ff_cent(x_cent_)
-        x_cent_ = self.dropout(x_cent_)
-        x_cent_ = torch.nn.functional.normalize(x_cent_ + x_cent, p=2, dim=1)
+        return torch.cat((x_window, x_cent), dim=1)
 
-        return torch.cat(tuple(x_prev + [x_cent_] + x_next), dim=1)
-        
 
 class PTFinalLayer(nn.Module):
     def __init__(self, in_dim, window):
         super(PTFinalLayer, self).__init__()
         self.IN_DIM = in_dim
         self.WINDOW = window
-        self.q = nn.Linear(self.IN_DIM, self.IN_DIM)
-        self.k_prev = nn.ModuleList([nn.Linear(self.IN_DIM, self.IN_DIM) for _ in range(window)])
-        self.k_cent = nn.Linear(self.IN_DIM, self.IN_DIM)
-        self.k_next = nn.ModuleList([nn.Linear(self.IN_DIM, self.IN_DIM) for _ in range(window)])
- 
+        self.q = nn.Linear(in_dim, in_dim)
+        self.k = nn.Conv1d(window + 1, (window + 1) * in_dim, in_dim, stride=in_dim, groups=(window + 1))
+
     def forward(self, x):
-        x_split = torch.split(x, self.IN_DIM, dim=1)
-        x_prev = x_split[:self.WINDOW]
-        x_cent = x_split[self.WINDOW]
-        x_next = x_split[self.WINDOW + 1:]
+        q = self.q(x[:, -1, :])
+        k = self.k(x).reshape(x.size(0), self.WINDOW + 1, self.IN_DIM)
+        attns = torch.matmul(q.unsqueeze(1), torch.transpose(k, -2, -1))
+        attns = torch.softmax(attns, dim=2)
+        x_cent = torch.matmul(attns, x)  # OG
+        # x_cent = torch.matmul(attns, x) + x[:, -1, :].unsqueeze(1)
 
-        q = self.q(x_cent)
-        attn_prev = [dot(q, self.k_prev[i](x_prev[i])) for i in range(self.WINDOW)]
-        attn_cent = dot(q, self.k_cent(x_cent))
-        attn_next = [dot(q, self.k_next[i](x_next[i])) for i in range(self.WINDOW)]
+        return torch.nn.functional.normalize(x_cent, p=2, dim=2)
 
-        attn = torch.cat(tuple(attn_prev + [attn_cent] + attn_next), dim=1)
-        attn = torch.softmax(attn, dim=1)
-        attn_split = torch.split(attn, 1, dim=1)
-
-        attn_prev_ = attn_split[:self.WINDOW]
-        attn_cent_ = attn_split[self.WINDOW]
-        attn_next_ = attn_split[self.WINDOW + 1:]
-
-        x_cent = x_cent * attn_cent_
-
-        for i in range(self.WINDOW):
-            x_cent = x_cent + (x_prev[i] * attn_prev_[i]) + (x_next[i] * attn_next_[i])
-
-        return torch.nn.functional.normalize(x_cent, p=2, dim=1)
-        
 
 class PositionalTransformer(nn.Module):
     def __init__(self, d_model=128, n_layers=3, window=1, p=0.1):
         super(PositionalTransformer, self).__init__()
         self.layers = nn.ModuleList([PTLayer(d_model, window, p) for _ in range(n_layers)])
         self.final_layer = PTFinalLayer(d_model, window)
-  
+
     def forward(self, x):
-        for i in range(len(self.layers)):
-            x = self.layers[i](x)
+        for layer in self.layers:
+            x = layer(x)
 
         return self.final_layer(x)
